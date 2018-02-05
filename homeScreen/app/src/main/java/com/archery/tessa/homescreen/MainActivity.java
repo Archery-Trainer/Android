b4852d35a9bde83401e0630a5a4f5a0688e84eec
Timo <jootimo@gmail.com>
2018-02-05 20:07:52 +0200

    Revert "Made mistakes, Revert "Merge branch 'master' into recording""
    This reverts commit 502d6d4019f127c5d15df08b19219552322886c4.

--- homeScreen/app/src/main/java/com/archery/tessa/homescreen/MainActivity.java
+++ homeScreen/app/src/main/java/com/archery/tessa/homescreen/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button5 = (Button) findViewById(R.id.button5);
-        button5.setOnClickListener(new View.OnClickListener() {
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SessionActivity.class);
@@ -24,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button settings = (Button) findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
@@ -33,14 +35,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class);
                 startActivity(intent);
             }
         });
 
 
-}}
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- homeScreen/app/src/main/java/com/archery/tessa/homescreen/RecordingActivity.java
+++ homeScreen/app/src/main/java/com/archery/tessa/homescreen/RecordingActivity.java
@@ -1,50 +1,31 @@
 package com.archery.tessa.homescreen;
 
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Handler;
-import android.speech.RecognitionListener;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;;
-import android.util.Log;
-import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.archery.tessa.homescreen.models.MeasuredDataSet;
-import com.archery.tessa.homescreen.models.RecordingRequest;
-import com.archery.tessa.homescreen.models.SensorData;
-import com.archery.tessa.homescreen.tasks.StartRecordingTask;
-import com.archery.tessa.homescreen.tasks.StopRecordingTask;
 import com.google.gson.Gson;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
 
-import mqttClient.AddToCollectionCallback;
 import mqttClient.MqttClient;
-import mqttClient.MqttMessageHandler;
 import mqttClient.OnMessageCallback;
 
-import static mqttClient.MqttMessageHandler.getNewestMessage;
 
+public class RecordingActivity extends AppCompatActivity implements OnMessageCallback {
 
-public class SessionActivity extends AppCompatActivity implements OnMessageCallback {
 
     private TextView txtv1;
     private TextView txtv2;
@@ -62,6 +43,11 @@ public class SessionActivity extends AppCompatActivity implements OnMessageCallb
     private GraphView graphView;
     private LineGraphSeries mSeries1;
     private LineGraphSeries mSeries2;
+    private LineGraphSeries mSeries3;
+    private LineGraphSeries mSeries4;
+    private LineGraphSeries mSeries5;
+    private LineGraphSeries mSeries6;
+
     private List<MeasuredDataSet> measuredDataPoints;
     private Gson gson;
     private OurView surfaceView;
@@ -75,45 +61,67 @@ public class SessionActivity extends AppCompatActivity implements OnMessageCallb
     //max value in views
     //final int LIMIT = 700;
 
-    public static Context context;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         /** Creating bitmap from archer picture**/
         BitmapFactory.Options options=new BitmapFactory.Options();
         options.inMutable=true;
         archerPic = BitmapFactory.decodeResource(getResources(),R.drawable.archer_ind_right_2_2_18v3,options);
-        setContentView(R.layout.session_activity);
-
-        context = this;
-      
-        //Initialize the 'recording' switch
-        setRecordingSwitch();
-      
+        setContentView(R.layout.recording_activity);
         surfaceView = (OurView)findViewById(R.id.archerSurfaceView);
         surfaceView.setdPicsForDrawing(archerPic);
 
         measuredDataPoints = new LinkedList<>();
         graphView = (GraphView) findViewById(R.id.graph);
         mSeries1=new LineGraphSeries<>();
+        mSeries1.setColor(Color.GREEN);
         mSeries1.setDrawAsPath(true);
-        mSeries2 = new LineGraphSeries<>();
+        mSeries1.setThickness(3);
 
+
+        mSeries2 = new LineGraphSeries<>();
+        mSeries2.setColor(Color.YELLOW);
         mSeries2.setDrawAsPath(true);
+        mSeries2.setThickness(3);
+
+
+        mSeries3=new LineGraphSeries<>();
+        mSeries3.setColor(Color.BLUE);
+        mSeries3.setDrawAsPath(true);
+        mSeries3.setThickness(3);
+
+
+        mSeries4 = new LineGraphSeries<>();
+        mSeries4.setColor(Color.CYAN);
+        mSeries4.setDrawAsPath(true);
+        mSeries4.setThickness(3);
+
+
+        mSeries5=new LineGraphSeries<>();
+        mSeries5.setColor(Color.RED);
+        mSeries5.setDrawAsPath(true);
+        mSeries5.setThickness(3);
+
+
+        mSeries6 = new LineGraphSeries<>();
+        mSeries6.setColor(Color.BLACK);
+        mSeries6.setDrawAsPath(true);
+        mSeries6.setThickness(3);
+
+
         graphView.addSeries(mSeries1);
         graphView.addSeries(mSeries2);
+        graphView.addSeries(mSeries3);
+        graphView.addSeries(mSeries4);
+        graphView.addSeries(mSeries5);
+        graphView.addSeries(mSeries6);
         graphView.getViewport().setXAxisBoundsManual(true);
         graphView.getViewport().setMinX(0);
         graphView.getViewport().setMaxX(100);
 
-    }
 
-    @Override
-    protected void onResume() {
-        //msgHandler = new MqttMessageHandler(getApplication());
         messages = new LinkedList<>();
 
 
@@ -142,8 +150,41 @@ public class SessionActivity extends AppCompatActivity implements OnMessageCallb
 
         super.onResume();
 
-
     }
+
+//    @Override
+//    protected void onResume() {
+//        //msgHandler = new MqttMessageHandler(getApplication());
+//        messages = new LinkedList<>();
+//
+//
+//        //Callback that will be called when a message arrives
+//        //OnMessageCallback cb = new AddToCollectionCallback(messages);
+//
+//        System.out.println("Creating MQTT-client");
+//        MqttClient c = new MqttClient(this, getApplication());
+//        gson = new Gson();
+//        /** defining textboxes for sensor data **/
+//        txtv1 = findViewById(R.id.sensorval1);
+//
+//        txtv2 = findViewById(R.id.sensorval2);
+//        txtv3 = findViewById(R.id.sensorval3);
+//        txtv4 = findViewById(R.id.sensorval4);
+//        txtv5 = findViewById(R.id.sensorval5);
+//        txtv6 = findViewById(R.id.sensorval6);
+//
+//        /** defining textboxes for sensor data max values**/
+//        textvmax = findViewById(R.id.sensormaxval1);
+//        textvmax2 = findViewById(R.id.sensormaxval2);
+//        textvmax3 = findViewById(R.id.sensormaxval3);
+//        textvmax4 = findViewById(R.id.sensormaxval4);
+//        textvmax5 = findViewById(R.id.sensormaxval5);
+//        textvmax6 = findViewById(R.id.sensormaxval6);
+//
+//        super.onResume();
+//
+//
+//    }
 
     @Override
     public void call(final String message) {
@@ -194,29 +235,29 @@ public class SessionActivity extends AppCompatActivity implements OnMessageCallb
                             max3 = sensor3;
                         }
                         txtv3.setText(Integer.toString(sensor3));
-                        mSeries1.appendData(new DataPoint((double)currentTime,(double)sensor3),true,100);
+                        mSeries3.appendData(new DataPoint((double)currentTime,(double)sensor3),true,100);
 
 
                         if(sensor4 > max4) {
-                           textvmax4.setText(Integer.toString(max4));
-                           max4 = sensor4;
+                            textvmax4.setText(Integer.toString(max4));
+                            max4 = sensor4;
                         }
                         txtv4.setText(Integer.toString(sensor4));
-                        mSeries1.appendData(new DataPoint((double)currentTime,(double)sensor4),true,100);
+                        mSeries4.appendData(new DataPoint((double)currentTime,(double)sensor4),true,100);
 
                         if(sensor5> max5) {
-                           textvmax5.setText(Integer.toString(max5));
+                            textvmax5.setText(Integer.toString(max5));
                             max5 = sensor5;
                         }
                         txtv5.setText(Integer.toString(sensor5));
-                        mSeries1.appendData(new DataPoint((double)currentTime,(double)sensor5),true,100);
+                        mSeries5.appendData(new DataPoint((double)currentTime,(double)sensor5),true,100);
 
                         if(sensor6 > max6) {
                             textvmax6.setText(Integer.toString(max6));
                             max6 = sensor6;
                         }
                         txtv6.setText(Integer.toString(sensor6));
-                        mSeries1.appendData(new DataPoint((double)currentTime,(double)sensor6),true,100);
+                        mSeries6.appendData(new DataPoint((double)currentTime,(double)sensor6),true,100);
                     }
                 });
             }
@@ -226,62 +267,4 @@ public class SessionActivity extends AppCompatActivity implements OnMessageCallb
 
     }//end call
 
-
-    /**
-     * Create a click listener for the recording switch
-     */
-    private void setRecordingSwitch() {
-        Switch recordingSwitch = (Switch) findViewById(R.id.switch1);
-
-        recordingSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                if(checked) {
-
-                    //@TODO: get current archer's email
-                    RecordingRequest req = new RecordingRequest("test@test.com", new Date().getTime());
-
-                    StartRecordingTask task = new StartRecordingTask(req);
-
-                    HttpStatus response = HttpStatus.BAD_REQUEST;
-                    try {
-                        response = task.execute(context).get();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    }
-
-                    if(response == HttpStatus.OK)
-                        System.out.println("Successfully started recording");
-                    //@TODO: Show error message
-                    else
-                        System.out.println("Got status " + response.toString() + " when starting recoding");
-
-                }
-
-                else {
-                    StopRecordingTask task = new StopRecordingTask();
-
-                    HttpStatus response = HttpStatus.BAD_REQUEST;
-
-                    try {
-                        response = task.execute(context).get();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    }
-
-                    if(response == HttpStatus.OK)
-                        System.out.println("Successfully stopped recording");
-                    else
-                        //@TODO: show error message
-                       System.out.println("Got status " + response.toString() + " when stopping recoding");
-                }
-
-
-            }
-        });
-    }
 }// end sessionactivity
